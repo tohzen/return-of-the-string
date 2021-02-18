ce37aaee8a4e253e555c675bcf56870558d5795f
Tohzen <melvin.l.mobley@gmail.com>
2021-02-18 13:41:18 -0500

    finished

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
+function yell (str) {
+  output = str + "!"
+  return output
 
+}
 
+function getFirstCharacter (str) {
+  output = str [0]
+  return output
 
+}
+
+function getLastCharacter (str) {
+  output = str[str.length-1]
+  return output
+
+}
+
+function getOneCharacter (str,num1) {
+  output = str[num1]
+  return output
+
+}
+
+function getTwoCharacters (str, num1 , num2) {
+  output = str[num1] + str[num2]
+  return output
+
+}
+
+function makeCapitalized (str) {
+  output = str.toUpperCase()
+  return output
+
+}
+
+function yellLouder (str) {
+  output = str.toUpperCase() + "!!!"
+  return output
+
+}
+
+function getInitials (str) {
+  const fullName = str.split(" ");
+  const initials = fullName.shift().charAt(0) + "." + fullName.pop().charAt(0) + ".";
+  return initials.toUpperCase();
+
+}
 
 
 
